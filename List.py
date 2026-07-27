22afe43ef9ce05126eaa439d330a9991a6fc1472
Drishti Sharma <sharmadrishti610@gmail.com>
2026-07-27 17:19:34 +0530

    Update List.py

--- List.py
+++ List.py
@@ -1,58 +1,194 @@
-1. Write a Python program to sum all the items in a list.  
+# 1. Write a Python program to sum all the items in a list.  
+
+s=[1,2,3,4,5]
+print(sum(s))
   
-2. Write a Python program to multiplies all the items in a list.  
+# 2. Write a Python program to multiplies all the items in a list.
+
+l = [2, 3, 4, 5]
+m = 1
+for i in l:
+    m*= i
+print("Product =", m)
   
-3. Write a Python program to get the largest number from a list.  
+# 3. Write a Python program to get the largest number from a list.
+
+l=[1,7,3,4,5]
+m=l[0]
+for i in range(len(l)):
+    if l[i]>m:
+        m=l[i]    
+print("largest number:",m)
   
-4. Write a Python program to get the smallest number from a list.  
+# 4. Write a Python program to get the smallest number from a list.  
+
+l=[1,7,3,4,5]
+m=l[0]
+for i in range(len(l)):
+    if l[i]<m:
+        m=l[i]    
+print("smallest number:",m)
   
-5. Write a Python program to count the number of strings where the string length is 2 or more and the first and last character are same from a given list of strings.  
+# 5. Write a Python program to count the number of strings where the string length is 2 or more and the first and last character are same from a given list of strings.  
 Sample List : ['abc', 'xyz', 'aba', '1221']
 Expected Result : 2
-  
+
+l=['abc', 'xyz', 'aba', '1221']
+c=0
+for i in l:
+    if len(i)>2 and i[0]==i[-1]:
+        c=c+1
+print(c)
+
 6. Write a Python program to get a list, sorted in increasing order by the last element in each tuple from a given list of non-empty tuples.  
 Sample List : [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
 Expected Result : [(2, 1), (1, 2), (2, 3), (4, 4), (2, 5)]
   
-7. Write a Python program to remove duplicates from a list.  
+# 7. Write a Python program to remove duplicates from a list.  
+
+l=[1,2,3,4,3,4]
+for i in l:
+    if l.count(i)>1:
+        l.remove(i)
+print(l)
+
+l = [1, 2, 3, 4, 3, 4]
+new = []
+for i in l:
+    if i not in new:
+        new.append(i)
+print(new)
   
-8. Write a Python program to check a list is empty or not.  
+# 8. Write a Python program to check a list is empty or not. 
+
+l = [1, 2]
+if l == []:
+    print("Empty List")
+else:
+    print("List is not empty")
   
-9. Write a Python program to clone or copy a list.  
+# 9. Write a Python program to clone or copy a list.  
+
+l = [1, 2]
+copy_list = l.copy()
+print(copy_list)
   
-10. Write a Python program to find the list of words that are longer than n from a given list of words.  
+# 10. Write a Python program to find the list of words that are longer than n from a given list of words. 
+
+l = ["My", "Name", "is", "drishti", "sharma"]
+new = []
+n = int(input("Enter the size of word: "))
+for i in l:
+    if len(i) > n:
+        new.append(i)
+print(new)
   
-11. Write a Python function that takes two lists and returns True if they have at least one common member.  
+# 11. Write a Python function that takes two lists and returns True if they have at least one common member.  
+
+l1=input("Enter the list:").split()
+l2=input("Enter the list:")).split()
+f=False
+for i in l1:
+    if i in l2:
+        f=True
+        break
+print(f)
   
-12. Write a Python program to print a specified list after removing the 0th, 4th and 5th elements.  
-Sample List : ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
-Expected Output : ['Green', 'White', 'Black']
+# 12. Write a Python program to print a specified list after removing the 0th, 4th and 5th elements.  
+# Sample List : ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+# Expected Output : ['Green', 'White', 'Black']
+
+l = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+for i in [5, 4, 0]:
+    l.pop(i)
+print(l)
+
   
-13. Write a Python program to generate a 3*4*6 3D array whose each element is *.  
+# 13. Write a Python program to generate a 3*4*6 3D array whose each element is *.  
   
-14. Write a Python program to print the numbers of a specified list after removing even numbers from it.  
+# 14. Write a Python program to print the numbers of a specified list after removing even numbers from it.  
+
+l = [1, 2, 3, 4, 5, 6]
+new = []
+for i in l:
+    if i % 2 != 0:
+        new.append(i)
+print(new)
   
-15. Write a Python program to shuffle and print a specified list.  
+# 15. Write a Python program to shuffle and print a specified list.  
+
+import random
+l = [1, 2, 3, 4, 5]
+random.shuffle(l)
+print(l)
   
-16. Write a Python program to generate and print a list of first and last 5 elements where the values are square of numbers between 1 and 30 (both included).  
+# 16. Write a Python program to generate and print a list of first and last 5 elements where the values are square of numbers between 1 and 30 (both included).  
+
+l = []
+for i in range(1, 31):
+    l.append(i * i)
+print("First 5 elements:", l[:5])
+print("Last 5 elements:", l[-5:])
   
-17. Write a Python program to generate and print a list except for the first 5 elements, where the values are square of numbers between 1 and 30 (both included).  
+# 17. Write a Python program to generate and print a list except for the first 5 elements, where the values are square of numbers between 1 and 30 (both included).  
+
+l = []
+for i in range(1, 31):
+    l.append(i * i)
+print(l[5:])
+
+# 18. Write a Python program to generate all permutations of a list in Python.  
   
-18. Write a Python program to generate all permutations of a list in Python.  
+# 19. Write a Python program to get the difference between the two lists.  
+
+l1 = [1, 2, 3, 4, 5]
+l2 = [0, 1, 3, 4, 7]
+l3 = []
+for i in l1:
+    if i not in l2:
+        l3.append(i)
+print(l3)
   
-19. Write a Python program to get the difference between the two lists.  
+# 20. Write a Python program access the index of a list.  
+
+l = [10, 20, 30, 40]
+for i in range(len(l)):
+    print("Index:", i, "Element:", l[i])
+
   
-20. Write a Python program access the index of a list.  
+# 21. Write a Python program to convert a list of characters into a string.  
+
+l = ['D', 'r', 'i', 's', 'h', 't', 'i']
+s = "".join(l)
+print(s)
+
+# 22. Write a Python program to find the index of an item in a specified list.  
+
+l = [10, 20, 30, 40]
+item = int(input("Enter item: "))
+print(l.index(item))
+
+# 23. Write a Python program to flatten a shallow list.  
+
+l = [[1, 2], [3, 4], [5, 6]]
+new = []
+
+
+
+
+for i in l:
+    for j in i:
+        new.append(j)
+print(new)
   
-21. Write a Python program to convert a list of characters into a string.  
+# 24. Write a Python program to append a list to the second list.  
+
+l1 = [1, 2, 3]
+l2 = [4, 5, 6]
+l2.append(l1)
+print(l2)
   
-22. Write a Python program to find the index of an item in a specified list.  
-  
-23. Write a Python program to flatten a shallow list.  
-  
-24. Write a Python program to append a list to the second list.  
-  
-25. Write a Python program to select an item randomly from a list.  
+# 25. Write a Python program to select an item randomly from a list.  
   
 26. Write a python program to check whether two lists are circularly identical.  
   
